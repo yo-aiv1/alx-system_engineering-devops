877fc11bdb3fda076c1d82b8d09b441508280c19
Yahya <yahyaoulha0@gmail.com>
2024-01-19 16:52:49 +0100

    worked

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#edit ssh config file
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
