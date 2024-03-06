e47fa885f3e1d4df24107d1b62f67a6ad0ddb7cb
Yahya <yahyaoulha0@gmail.com>
2024-03-06 17:46:24 +0100

    worked

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Fetch Data from reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Get the titles of the first 10 hot posts
+    listed for a given subreddit
+    """
+
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'limit': 10
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+    if res.status_code != 200:
+        print(None)
+        return
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    if len(hot_posts) is 0:
+        print(None)
+    else:
+        for post in hot_posts:
+            print(post['data']['title'])
