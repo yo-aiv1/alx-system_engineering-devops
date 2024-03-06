39c406730aa3c8e625f4213c2331f373337c08c9
Yahya <yahyaoulha0@gmail.com>
2024-03-06 17:42:40 +0100

    worked

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,17 +6,19 @@ import requests
 def number_of_subscribers(subreddit):
     """Get subscribe count of a subreddit"""
 
-    api = "https://www.reddit.com//r/{}/about.json".format(subreddit)
     u_agent = 'Mozilla/5.0'
-    headers = {'User-Agent': u_agent}
 
-    response = requests.get(api, headers=headers, allow_redirects=False)
+    headers = {
+        'User-Agent': u_agent
+    }
 
-    if response.status_code == 200:
-        JsonObj = response.json()
-        try:
-            return JsonObj["data"]["subscribers"]
-        except:
-            return 0
-
-    return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
